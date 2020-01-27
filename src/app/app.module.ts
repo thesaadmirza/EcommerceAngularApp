59e90413885732c90bf8030d1014027557520f1f
Saad Mirza <saadmirza009@gmail.com>
2020-01-27 23:22:55 +0500

    Add recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { RecipiesComponent } from './recipies/recipies.component';
+
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipiesComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipesComponent,
+    RecipeDetailComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
